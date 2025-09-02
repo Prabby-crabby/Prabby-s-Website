093656ef9ce5bf5be21886869f8137e91c4e1f35
Prabby <s4168305@student.rmit.edu.au>
2025-09-02 20:12:30 +1000

    styles changes

---
+++ script.js
@@ -0,0 +1,21 @@
+/*console.log("Hello, World!");
+console.log("Test")
+document.getElementById("myH1").textContent = "Hello";
+window.alert("You are entering Prabby's Website!");
+
+let money = 5;
+let cost = 10;
+let total = money + cost;
+console.log(`these are examples: ${money}, ${cost}, ${total}`);
+*/
+//let name = "Prabby";
+//console.log(`my name is ${name}`)
+
+/*let online = true;
+let offline = false;
+console.log(`I am online: ${online}`);
+console.log(`I am offline: ${offline}`);
+
+let username = window.prompt("Enter your username: ");
+console.log(username);
+*/
